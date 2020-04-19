6e50d164e05e16aa2a68d463a2e9bfeeb71afd8c
ionimp3 <ionimp3@gmail.com>
2020-04-19 16:34:28 +0900

    Initial commit

---
+++ app/src/main/java/com/lmh/mytraveldairyjava/DairyNew.java
@@ -0,0 +1,18 @@
+package com.lmh.mytraveldairyjava;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DairyNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dairy_new);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/lmh/mytraveldairyjava/StartActivity.java
@@ -0,0 +1,35 @@
+package com.lmh.mytraveldairyjava;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class StartActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+       ImageButton imageButton = (ImageButton) findViewById(R.id.btnnewplan);
+
+       imageButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(), DairyNew.class);
+               startActivity(intent);
+           }
+       });
+
+    }
+}
