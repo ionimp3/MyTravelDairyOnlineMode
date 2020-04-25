567938ecd3ae7ebdff94e500d3dad41ed9377172
ionimp3 <ionimp3@gmail.com>
2020-04-25 10:41:52 +0900

    슬라이드화면 애니메이션 추가

--- app/src/main/java/com/lmh/mytraveldairyjava/OnBoarding.java
+++ app/src/main/java/com/lmh/mytraveldairyjava/OnBoarding.java
@@ -5,6 +5,10 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,37 +19,43 @@ public class OnBoarding extends AppCompatActivity {
 
     ViewPager viewPager;
     LinearLayout dotsLayout;
-    Button bt_my01 , bt_my02;
+    Button bt_my01, bt_my02, play_Onboarding;
 
     SliderAdapter sliderAdapter;
     TextView[] dots;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
 
         // hook
         viewPager = findViewById(R.id.onBoarding_ViewPager);
         dotsLayout = findViewById(R.id.dots);
-        bt_my01 = findViewById(R.id.mybt01);
-        bt_my02 = findViewById(R.id.mybt02);
+        bt_my01 = findViewById(R.id.mybtLogin);
+        bt_my02 = findViewById(R.id.mybtRegist);
+        play_Onboarding = findViewById(R.id.onBoarding_play);
+
 
         //call adaptor
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
 
-        addDots(0);;
+        addDots(0);
+        ;
         viewPager.addOnPageChangeListener(changeListener);
     }
 
-    private  void  addDots(int position){
+    private void addDots(int position) {
 
         //변형된 특수문자 . 를 4개 그려주는 구문..좀더 확인 필요
         dots = new TextView[4];
-        dotsLayout.removeAllViews();;
+        dotsLayout.removeAllViews();
+        ;
 
-        for (int i=0; i<dots.length;i++){
+        for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
             // &#8226 내장된 특수문자(변형된 .)
             dots[i].setText(Html.fromHtml("&#8226;"));
@@ -55,7 +65,7 @@ public class OnBoarding extends AppCompatActivity {
         }
 
         //슬라이드 페이지 변할때 아래 . 글자 색상 바꾸기
-        if(dots.length > 0){
+        if (dots.length > 0) {
             dots[position].setTextColor(getResources().getColor(R.color.whiteColor));
         }
     }
@@ -71,15 +81,39 @@ public class OnBoarding extends AppCompatActivity {
 
             addDots(position);
 
+            if (position == 0) {
+                animation = AnimationUtils.loadAnimation(OnBoarding.this,R.anim.an2_bottom_animation);
+                bt_my01.setAnimation(animation);
+                bt_my02.setAnimation(animation);
+                play_Onboarding.setVisibility(View.VISIBLE);
+                bt_my01.setVisibility(View.VISIBLE);
+                bt_my02.setVisibility(View.VISIBLE);
+            } else if (position == 1) {
+                play_Onboarding.setVisibility(View.INVISIBLE);
+                bt_my01.setVisibility(View.INVISIBLE);
+                bt_my02.setVisibility(View.INVISIBLE);
+            } else if (position == 2) {
+                play_Onboarding.setVisibility(View.INVISIBLE);
+                bt_my01.setVisibility(View.INVISIBLE);
+                bt_my02.setVisibility(View.INVISIBLE);
+            } else {
+                animation = AnimationUtils.loadAnimation(OnBoarding.this,R.anim.an2_side_animation);
+                bt_my01.setAnimation(animation);
+                bt_my02.setAnimation(animation);
+
+                play_Onboarding.setVisibility(View.VISIBLE);
+                bt_my01.setVisibility(View.VISIBLE);
+                bt_my02.setVisibility(View.VISIBLE);
+
+            }
         }
 
-        @Override
-        public void onPageScrollStateChanged(int state) {
 
-        }
-    };
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
-
+    }
+};
 
 
 }
