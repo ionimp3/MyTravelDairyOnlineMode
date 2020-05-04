b63af3528b917d0feee0710816fd49cc46412ff6
ionimp3 <ionimp3@gmail.com>
2020-05-04 19:19:10 +0900

    정상사용자인지 체크 및 백버튼 메세지 추가

--- app/src/main/java/DashBoard/UserDashboard.java
+++ app/src/main/java/DashBoard/UserDashboard.java
@@ -18,13 +18,25 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.lmh.mytraveldairyjava.R;
 
+import Common.BackPressHandler;
+import Common.OnBoarding;
 import Common.SignInActivity;
 import Profile.ProfileActivity;
 
 
 public class UserDashboard extends AppCompatActivity {
+    // BackPressHandler 객체 선언, 할당
+    private BackPressHandler backPressHandler = new BackPressHandler(this);
 
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
@@ -32,11 +44,27 @@ public class UserDashboard extends AppCompatActivity {
     private RecyclerView postList;
     private Toolbar mToolbar;
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private StorageReference UserProfileImageRef;
+    String currentUserId;
+    String psemail,tmps1,tmps2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboardlayout);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        FirebaseUser user = mAuth.getCurrentUser();
+        psemail = user.getEmail();
+        tmps1 = psemail.replaceAll("[.]", "");
+        tmps2 = tmps1.replaceAll("[@]", "");
+        UsersRef = FirebaseDatabase.getInstance().getReference().child(tmps2).child("userProfile");
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child(tmps2).child("userProfileImages");
+
 
         mToolbar = (Toolbar)findViewById(R.id.dashboard_main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -64,6 +92,42 @@ public class UserDashboard extends AppCompatActivity {
         });
 
     }
+    //실제등록접속되었는지 DB에 쿼리 확인
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            Intent userDashBoardIntent = new Intent(UserDashboard.this, OnBoarding.class);
+            userDashBoardIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(userDashBoardIntent);
+            finish();
+        } else {
+            CheckUserExistance();
+        }
+    }
+    private void CheckUserExistance() {
+        final String current_User_Id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference RealUserCheck = FirebaseDatabase.getInstance().getReference();
+        RealUserCheck.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //실제 데이터베이스를 조회해서 이메일 id 가 있는지 확인
+                if (!dataSnapshot.hasChild(current_User_Id)) {
+                    Intent userDashBoardIntent = new Intent(UserDashboard.this, OnBoarding.class);
+                    userDashBoardIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(userDashBoardIntent);
+                    finish();
+                }
+                else{
+                    //MessageToast.message(UpdateNicName.this,"실제접속했어요");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     //툴바 뒤로가기 버튼에 동작을 부여
     @Override
@@ -78,10 +142,8 @@ public class UserDashboard extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.nav_home:
                 drawerLayout.closeDrawer(GravityCompat.START);
-                Toast.makeText(UserDashboard.this, "홈.", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_setting:
-                Toast.makeText(UserDashboard.this, "설정.", Toast.LENGTH_SHORT).show();
                 Intent ProfileIntent = new Intent(this, ProfileActivity.class);
                 ProfileIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(ProfileIntent);
@@ -89,12 +151,19 @@ public class UserDashboard extends AppCompatActivity {
                 break;
             case R.id.nav_logout:
                 FirebaseAuth.getInstance().signOut();
-                Toast.makeText(UserDashboard.this, "로그아웃 하였읍니다.", Toast.LENGTH_SHORT).show();
                 Intent logoutIntent = new Intent(UserDashboard.this, SignInActivity.class);
                 logoutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(logoutIntent);
                 finish();
                 break;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            backPressHandler.onBackPressed("뒤로가기 버튼 한번 더 누르면 앱을 종료");
         }
     }
 }
